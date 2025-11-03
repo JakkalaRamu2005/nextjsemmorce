9eacac4b0a3f0eb763e5cc73ef82ebb65b8af293
JakkalaRamu2005 <jakkalaramu233@gmail.com>
2025-11-03 11:51:03 +0530

    workingwithmongo

---
+++ fullstacknextjs/nextjsapp/src/app/page.js
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1>hello world</h1>
+
+      {/*  */}
+      {/* 
+ 
+
+      
+      */}
+    </div>
+  )
+}
+
+export default page
+
